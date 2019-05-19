994210fac1536b716ea992c2901c5c936a3a5bc2
노성현 <shashum@daum.net>
2019-05-19 16:21:26 +0900

    천원 줘야지

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('home/', views.home, name = 'home'),
+    path('post/<int:post_id>', views.detail, name = 'detail'),
+    path('post/new/', views.post_new, name='new'),
+    path('post/<int:post_id>/edit/', views.post_edit, name = 'edit'),
+    path('post/<int:post_id>/delete/', views.post_delete, name = 'delete'),
+    path('post/<int:post_id>/comment/', views.add_comment, name = 'add_comment'),
+    path('comment/<int:comment_id>/delete/', views.comment_delete, name = 'comment_delete'),
+    
+]
